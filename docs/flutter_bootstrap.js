f8b35633ff9747a55b400f37dbe300c97120ceb2
Yassir-9 <yaso0ore20@gmail.com>
2026-07-16 20:26:08 +0000

    إصلاح جذري: تصحيح base-href ليطابق مسار GitHub Pages الفرعي

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1289899203" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1091669073" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
