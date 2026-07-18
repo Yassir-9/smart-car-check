5229b8ba2e806ffdf4ad71474e16e774d1a902a0
Yassir-9 <yaso0ore20@gmail.com>
2026-07-18 11:05:11 +0000

    نشر نهائي شامل: تنبيهات ذكية، Health Score، إحصائيات

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "905008358" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1303597559" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
